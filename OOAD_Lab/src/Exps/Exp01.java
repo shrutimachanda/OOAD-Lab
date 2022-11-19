e1b0b9e3ee2ba83a76e96994aeebbfdc2e0f5cbf
Shruti Manchanda <shruti.manchanda@sap.com>
2022-11-19 10:01:22 +0530

    Demo Java project

---
+++ OOAD_Lab/src/Exps/Exp01.java
@@ -0,0 +1,10 @@
+package Exps;
+import java.util.*;
+public class Exp01 {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("Hello World!");
+	}
+
+}
---
+++ OOAD_Lab/src/module-info.java
@@ -0,0 +1,3 @@
+module OOAD_Lab {
+	
+}
